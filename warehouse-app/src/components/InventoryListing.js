03772018e57c33e50246aa1dcc10c80f9eb983b0
chwrd <hocheng06@gmail.com>
2024-03-26 18:11:00 -0700

    finish add css

--- warehouse-app/src/components/InventoryListing.js
+++ warehouse-app/src/components/InventoryListing.js
@@ -1,8 +1,13 @@
 import InventoryListingItem from "./InventoryListingItem";
 
 export default function InventoryListing() {
+    const myStyle = {
+        margin: "0 auto",
+        width: "30vw",
+        border: "0.2vw solid black"
+    };
     return (
-        <div>
+        <div style = {myStyle}>
             <InventoryListingItem/>
             <InventoryListingItem/>
             <InventoryListingItem/>
--- warehouse-app/src/components/InventoryListingItem.js
+++ warehouse-app/src/components/InventoryListingItem.js
@@ -1,6 +1,8 @@
+import './InventoryListingItem.css'
+
 export default function InventoryListingItem() {
     return (
-        <div>
+        <div className='listing-item'>
             <span className="listing-item-name">Socks</span><span className="listing-item-count">10</span>
         </div>
     );
