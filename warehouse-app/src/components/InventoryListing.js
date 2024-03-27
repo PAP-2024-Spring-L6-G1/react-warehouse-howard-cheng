9797f796c62ae708a91c8951dab6612cfc264458
chwrd <hocheng06@gmail.com>
2024-03-26 17:15:40 -0700

    finish inventory listing

---
+++ warehouse-app/src/components/InventoryListing.js
@@ -0,0 +1,18 @@
+import InventoryListingItem from "./InventoryListingItem";
+
+export default function InventoryListing() {
+    return (
+        <div>
+            <InventoryListingItem/>
+            <InventoryListingItem/>
+            <InventoryListingItem/>
+            <InventoryListingItem/>
+            <InventoryListingItem/>
+            <InventoryListingItem/>
+            <InventoryListingItem/>
+            <InventoryListingItem/>
+            <InventoryListingItem/>
+            <InventoryListingItem/>
+        </div>
+    );
+}
---
+++ warehouse-app/src/components/InventoryListingItem.js
@@ -0,0 +1,7 @@
+export default function InventoryListingItem() {
+    return (
+        <div>
+            <span className="listing-item-name">Socks</span><span className="listing-item-count">10</span>
+        </div>
+    );
+}
