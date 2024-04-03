0847e2d866950c954c5d12abf29044fc5fa9959e
chwrd <hocheng06@gmail.com>
2024-04-02 17:31:21 -0700

    finish react states

--- warehouse-app/src/components/InventoryListing.js
+++ warehouse-app/src/components/InventoryListing.js
@@ -1,19 +1,43 @@
 import InventoryListingItem from "./InventoryListingItem";
+import React, { useState } from 'react';
 
 export default function InventoryListing(props) {
+    const [currentPageIndex, setCurrentPageIndex] = useState(0);
+
     const myStyle = {
         margin: "0 auto",
         width: "30vw",
         border: "0.2vw solid black"
     };
+    const buttonStyle = {
+        width: "50%"
+    }
+
     const allListingItems = []
     for (var i = 0; i < 50; i++) {
-        allListingItems[i] = <InventoryListingItem/>;
-    } 
-    const visibleListingItems = allListingItems.slice(0, props.maxVisible)
+        allListingItems[i] = <InventoryListingItem count={i} />;
+    }
+    const visibleListingItems = allListingItems.slice(currentPageIndex, currentPageIndex + props.maxVisible)
+
+
+    function previousPage() {
+        if (currentPageIndex > 0) {
+            setCurrentPageIndex(currentPageIndex - props.maxVisible);
+        }
+    }
+
+    function nextPage() {
+        if (currentPageIndex + props.maxVisible < allListingItems.length) {
+            setCurrentPageIndex(currentPageIndex + props.maxVisible);
+        }
+    }
+
     return (
-        <div style = {myStyle}>
+        <div style={myStyle}>
             {visibleListingItems}
+            <button onClick={previousPage} style={buttonStyle}>Previous</button>
+            <button onClick={nextPage} style={buttonStyle}>Next</button>
         </div>
+
     );
 }
--- warehouse-app/src/components/InventoryListingItem.js
+++ warehouse-app/src/components/InventoryListingItem.js
@@ -1,9 +1,9 @@
 import './InventoryListingItem.css'
 
-export default function InventoryListingItem() {
+export default function InventoryListingItem(props) {
     return (
         <div className='listing-item'>
-            <span className="listing-item-name">Socks</span><span className="listing-item-count">10</span>
+            <span className="listing-item-name">Socks</span><span className="listing-item-count">{props.count}</span>
         </div>
     );
 }
