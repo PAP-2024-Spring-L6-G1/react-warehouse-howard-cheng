34c655bad9323fa9b99d37944b7c23329bb61435
chwrd <hocheng06@gmail.com>
2024-04-09 18:38:01 -0700

    added axios

--- warehouse-app/src/components/InventoryListing.js
+++ warehouse-app/src/components/InventoryListing.js
@@ -1,8 +1,18 @@
 import InventoryListingItem from "./InventoryListingItem";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'http://localhost:3005/',
+  });
+//instance.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 export default function InventoryListing(props) {
     const [currentPageIndex, setCurrentPageIndex] = useState(0);
+
+    const [inventory, setInventory] = useState([]);
+
+
 
     const myStyle = {
         margin: "0 auto",
@@ -14,9 +24,28 @@ export default function InventoryListing(props) {
     }
 
     const allListingItems = []
-    for (var i = 0; i < 50; i++) {
-        allListingItems[i] = <InventoryListingItem count={i} />;
+    for (var i = 0; i < inventory.length; i++) {
+        allListingItems[i] = <InventoryListingItem name={inventory[i].name} count={inventory[i].count} />;
     }
+
+
+    // const allListingItems = inventory.map((name, count, _id) => {
+    //     <div key={_id}>
+    //         <InventoryListingItem count={count}/>
+    //     </div>
+    // })
+    async function getAllItems() {
+        try {
+            const response = await instance.get('/inventory');
+            console.log(response.data);
+            setInventory(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+    useEffect(()=>{getAllItems();}, []);
+
     const visibleListingItems = allListingItems.slice(currentPageIndex, currentPageIndex + props.maxVisible)
 
 
--- warehouse-app/src/components/InventoryListingItem.js
+++ warehouse-app/src/components/InventoryListingItem.js
@@ -3,7 +3,7 @@ import './InventoryListingItem.css'
 export default function InventoryListingItem(props) {
     return (
         <div className='listing-item'>
-            <span className="listing-item-name">Socks</span><span className="listing-item-count">{props.count}</span>
+            <span className="listing-item-name">{props.name}</span><span className="listing-item-count">{props.count}</span>
         </div>
     );
 }
